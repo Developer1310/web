4cb10c71535ad5db17c070600d2321525f027b86
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-04-26 17:35:31 +0530

    structured

---
+++ project/pages/script.js
@@ -0,0 +1,57 @@
+const header = document.querySelector(".header");
+const about = document.querySelector("#about");
+const data_class = document.querySelector(".data");
+const footer = document.querySelector(".footer");
+document.querySelector(".main-title").textContent = all.name;
+const addHeader = () => {
+  let a = document.createElement("div");
+  let a1 = document.createElement("h1");
+  let a2 = document.createElement("a");
+  let a21 = document.createElement("button");
+  header.style.backgroundImage = `url(${all.imgLink})`;
+  a1.style.color = all.nameColor;
+  a.className = "text-vertical-center";
+  a.style.verticalAlign = all.namePosition;
+  a1.className = "title";
+  a1.textContent = all.name;
+  a2.href = "#about";
+  a21.textContent = "";
+  a2.appendChild(a21);
+  a.appendChild(a1);
+  a.appendChild(a2);
+  header.appendChild(a);
+};
+const addBody = () => {
+  const fullName = document.querySelector(".full-name");
+  const b_day = document.querySelector(".birth-date");
+  const location = document.querySelector(".location");
+  fullName.textContent = all.full_name;
+  b_day.textContent = all.birth_date;
+  location.textContent = all.address;
+};
+const addData = () => {
+  all.data.forEach((d) => {
+    let div = document.createElement("div");
+    let tmp = document.createElement("p");
+    let head = document.createElement("h3");
+    head.className = "heading";
+    tmp.textContent = d[1];
+    head.textContent = d[0];
+    div.appendChild(head);
+    div.appendChild(tmp);
+    data_class.appendChild(div);
+  });
+};
+const addFooter = () => {
+  let p1 = document.createElement("p");
+  let a1 = document.createElement("a");
+  a1.href = all.wikiLink;
+  a1.textContent = "click here!";
+  p1.textContent = "If you want to know more about " + all.name + " --> ";
+  p1.appendChild(a1);
+  footer.appendChild(p1);
+};
+addHeader();
+addBody();
+addData();
+addFooter();
---
+++ project/pages/stevejobs/data.js
@@ -0,0 +1,21 @@
+const all = {
+  name: "Steve Jobs",
+  full_name: "Steven Paul Jobs",
+  address: "San Francisco, California, U.S.A.",
+  birth_date: "February 24, 1955",
+  wikiLink: "https://en.wikipedia.org/wiki/Steve_Jobs",
+  imgLink:
+    "https://i.pinimg.com/originals/d8/58/be/d858be20d47147e83f9f7ba30a7eb6bf.jpg",
+  nameColor: "transparent",
+  namePosition: "bottom",
+  data: [
+    [
+      "Birth",
+      'Steven Paul Jobs was born on February 24, 1955, to Abdulfattah Jandali and Joanne Schieble, and was adopted by Paul and Clara Jobs.His biological father, Abdulfattah "John" (al-)Jandali (Arabic: عبد الفتاح الجندلي), grew up in Homs, Syria, and was born into an Arab Muslim household. While an undergraduate at the American University of Beirut, Lebanon, he was a student activist and spent time in prison for his political activities. He pursued a PhD at the University of Wisconsin, where he met Joanne Carole Schieble, a Catholic of Swiss and German descent. As a doctoral candidate, Jandali was a teaching assistant for a course Schieble was taking, although both were the same age. Mona Simpson, Jobs\'s biological sister, notes that her maternal grandparents were not happy that their daughter was dating a Muslim. Walter Isaacson, Steve Jobs\'s official biographer, additionally states that Schieble\'s father "threatened to cut Joanne off completely" if she continued the relationship.',
+    ],
+    [
+      "Adoptive Family",
+      "Jobs's adoptive father, Paul Reinhold Jobs, was a Coast Guard mechanic. After leaving the Coast Guard, Paul Jobs married Clara Hagopian in 1946. Their attempts to start a family were halted after Clara had an ectopic pregnancy, leading them to consider adoption in 1955.",
+    ],
+  ],
+};
