25871ab2cffbc2a133a07af9f719e9c1b792b700
IshitwaAgrawal <ishitwa2000@gmail.com>
2020-04-26 23:55:30 +0530

    3 left

--- project/pages/larrypage/data.js
+++ project/pages/larrypage/data.js
@@ -1,12 +1,33 @@
 const all = {
-  name: "",
-  full_name: "",
-  address: "",
-  birth_date: "",
-  wikiLink: "",
-  imgLink: "",
-  nameColor: "",
-  namePostion: "",
-  backImg: "",
-  data: [],
+  name: "Larry Page",
+  full_name: "Lawrence Edward Page",
+  address: "Lansing, Michigan, U.S.A.",
+  birth_date: "March 26, 1973",
+  wikiLink: "https://en.wikipedia.org/wiki/Larry_Page",
+  imgLink: "https://wallpapercave.com/wp/wp4171369.jpg",
+  nameColor: "white",
+  namePostion: "bottom",
+  backImg: "https://wallpapercave.com/wp/wp2860498.jpg",
+  data: [
+    [
+      "Early life and education",
+      'Page was born on March 26, 1973, in Lansing, Michigan. His mother is Jewish; his maternal grandfather later made aliyah to Israel. However, he does not declare to follow any formal religion. His father, Carl Victor Page Sr., earned a PhD in computer science from the University of Michigan, when the field was being established, and BBC reporter Will Smale has described him as a "pioneer in computer science and artificial intelligence". He was a computer science professor at Michigan State University and Page\'s mother, Gloria, was an instructor in computer programming at Lyman Briggs College of Michigan State University.After enrolling in a computer science PhD program at Stanford University, Page was in search of a dissertation theme and considered exploring the mathematical properties of the World Wide Web, understanding its link structure as a huge graph. His supervisor, Terry Winograd, encouraged him to pursue the idea, and Page recalled in 2008 that it was the best advice he had ever received. He also considered doing research on telepresence and self-driving cars during this time.',
+    ],
+    [
+      "Search engine development",
+      "To convert the backlink data gathered by BackRub's web crawler into a measure of importance for a given web page, Brin and Page developed the PageRank algorithm, and realized that it could be used to build a search engine far superior to existing ones. The algorithm relied on a new technology that analyzed the relevance of the backlinks that connected one web page to another.Combining their ideas, the pair began utilizing Page's dormitory room as a machine laboratory, and extracted spare parts from inexpensive computers to create a device that they used to connect the now nascent search engine with Stanford's broadband campus network. After filling Page's room with equipment, they then converted Brin's dorm room into an office and programming center, where they tested their new search engine designs on the Web. The rapid growth of their project caused Stanford's computing infrastructure to experience problems.",
+    ],
+    [
+      "Page and Sergey Brin",
+      "Page and Brin used the former's basic HTML programming skills to set up a simple search page for users, as they did not have a web page developer to create anything visually elaborate. They also began using any computer part they could find to assemble the necessary computing power to handle searches by multiple users. As their search engine grew in popularity among Stanford users, it required additional servers to process the queries. In August 1996, the initial version of Google, still on the Stanford University website, was made available to Internet users.",
+    ],
+    [
+      "Google",
+      'Soliciting funds from faculty members, family and friends, Brin and Page scraped together enough to buy some servers and rent that famous garage in Menlo Park. ... [soon after], Sun Microsystems co-founder Andy Bechtolsheim wrote a $100,000 check to "Google, Inc." The only problem was, "Google, Inc." did not yet exist�the company hadn\'t yet been incorporated. For two weeks, as they handled the paperwork, the young men had nowhere to deposit the money.In 1998, Brin and Page incorporated Google, Inc. with the initial domain name of "Googol", derived from a number that consists of one followed by one hundred zeros�representing the vast amount of data that the search engine was intended to explore. Following inception, Page appointed himself as CEO, while Brin, named Google\'s co-founder, served as Google\'s president. Writer Nicholas Carlson wrote in 2014:The pair\'s mission was "to organize the world\'s information and make it universally accessible and useful." With a US$1-million loan from friends and family, the inaugural team moved into a Mountain View office by the start of 2000. In 1999, Page experimented with smaller servers so Google could fit more into each square meter of the third-party warehouses the company rented for their servers. This eventually led to a search engine that ran much faster than Google\'s competitors at the time.By June 2000, Google had indexed one billion Internet URLs (Uniform Resource Locators), making it the most comprehensive search engine on the Web at the time. The company cited NEC Research Institute data in its June 26 press release, stating that "there are more than 1 billion web pages online today", with Google "providing access to 560 million full-text indexed web pages and 500 million partially indexed URLs.',
+    ],
+    [
+      "REWARDS AND RECOGNITION",
+      "In 2002, Page and Brin were named in the MIT Technology Review TR100 for being among the top 100 innovators in the world under the age of 35.The World Economic Forum named Page as a Global Leader for Tomorrow in 2002.Page and Brin received the prestigious Marconi Foundation Prize(2004) which is given to honour significant contributions in science and technology.They were also elected Fellow of the Marconi Foundation at Columbia University.",
+    ],
+  ],
 };
--- project/pages/sabeerbhatia/data.js
+++ project/pages/sabeerbhatia/data.js
@@ -1,12 +1,21 @@
 const all = {
-  name: "",
-  full_name: "",
-  address: "",
-  birth_date: "",
-  wikiLink: "",
-  imgLink: "",
+  name: "Sabeer Bhatia",
+  full_name: "Sabeer Bhatia",
+  address: "Chandigarh, India",
+  birth_date: "30 December 1968",
+  wikiLink: "https://en.wikipedia.org/wiki/Sabeer_Bhatia",
+  imgLink: "https://blazonsart.com/great-indians/sabeer-bhatia.jpg",
   nameColor: "",
-  namePostion: "",
+  namePostion: "bottom",
   backImg: "",
-  data: [],
+  data: [
+    [
+      "Career",
+      'Bhatia briefly worked for Apple Computer (as a hardware engineer) and Firepower Systems Inc. He, along with his colleague Jack Smith, set up Hotmail on 4 July 1996, American Independence Day, symbolizing "freedom" from ISP-based e-mail and the ability to access a user\'s inbox from anywhere in the world.             As President and CEO, Bhatia led Hotmail until its eventual acquisition by Microsoft in 1998 for an estimated $400 million. Bhatia worked at Microsoft for one year after the Hotmail acquisition and in April 1999, left Microsoft to start another venture, Arzoo Inc, an e-commerce firm.                          Bhatia started a free messaging service called JaxtrSMS. He said that JaxtrSMS would do to SMS what Hotmail did for e-mail. Claiming it to be a disruptive technology, he says that the operators will lose revenue on the reduction in number of SMSes on their network but will benefit from the data plan that the user has to buy. To date, JaxtrSMS service has failed to replicate the success of Hotmail. Recently[when?], he invested in email collaboration software, ccZen.',
+    ],
+    [
+      "Personal life",
+      'His father, Baldev Bhatia, was a captain in the Indian Army and his mother worked for the Central Bank of India.                                             Sabeer married Tanya Sharma in 2008 and they have a daughter together. Later, they filed for divorce in January 2013 in a court in San Francisco, citing "irreconcilable differences". ',
+    ],
+  ],
 };
